bf9c52bb8fe904faa3c2551a6af1388f265024cc
Actions User <actions@github.com>
2022-11-26 00:47:37 +0000

    [CI] Updating versions for 2022.11.26.2445

--- Blender/__init__.py
+++ Blender/__init__.py
@@ -2,7 +2,7 @@ bl_info = {
 	"name" : "XAT",
 	"author" : "Asgard",
 	"description" : "Animations for FFXIV",
-	"version": (1,0,0,0),
+	"version": (2022,11,26,2445),
 	"blender" : (3, 0, 0),
 	"location" : "3D View > Tools (Right Side) > XAT",
 	"warning" : "",
