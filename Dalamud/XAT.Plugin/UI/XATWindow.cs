008761c323b017f4a270d4ac8552207a399a51a2
Kenneth M <KennethMizell@outlook.com>
2024-08-13 22:19:54 -0500

    XAT migration Plugin

---
+++ Dalamud/XAT.Plugin/UI/XATWindow.cs
@@ -0,0 +1,76 @@
+﻿using Dalamud.Interface.Utility.Raii;
+using Dalamud.Interface.Windowing;
+using ImGuiNET;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace XAT.Plugin.UI;
+
+public class XATWindow : Window
+{
+    private XATPlugin _plugin { get; }
+
+    public XATWindow(XATPlugin plugin) : base($" {XATPlugin.PluginName} - Attention!", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse | ImGuiWindowFlags.NoResize)
+    {
+        _plugin = plugin;
+
+        SizeConstraints = new WindowSizeConstraints
+        {
+            MinimumSize = new Vector2(520, 265),
+            MaximumSize = new Vector2(520, 265)
+        };
+    }
+
+    public override void Draw()
+    {
+        var segmentSize = ImGui.GetWindowSize().X / 3.45f;
+        var buttonSize = new Vector2(segmentSize, ImGui.GetTextLineHeight() * 1.8f);
+
+        using (var textGroup = ImRaii.Group())
+        {
+            if (textGroup.Success)
+            {
+                var text = $"""
+
+                    The XAT Plugin has been merged with Brio 0.4.0!
+
+                    Once you have installed Brio you can find the XAT Cutscene Control
+                    under the `Advanced Animation Control` Window, found by clicking
+                    the 'point up-right' button on the Animation Control Tab!
+
+                    For additional help, you may DM me on Twitter/Discord or, join the Discord.
+
+                    Thank You!
+
+
+                    """;
+
+                ImGui.PushTextWrapPos(segmentSize * 3);
+                ImGui.TextWrapped(text);
+                ImGui.PopTextWrapPos();
+            }
+        }
+
+        using var buttonGroup = ImRaii.Group();
+        if (buttonGroup.Success)
+        {
+            ImGui.PushStyleColor(ImGuiCol.Button, new Vector4(255, 0, 0, 255) / 255);
+            if (ImGui.Button("Get Brio", buttonSize))
+                Process.Start(new ProcessStartInfo { FileName = "https://github.com/Etheirys/Brio/?tab=readme-ov-file#installation", UseShellExecute = true });
+            ImGui.PopStyleColor();
+            ImGui.SameLine();
+
+            ImGui.PushStyleColor(ImGuiCol.Button, new Vector4(86, 98, 246, 255) / 255);
+            if (ImGui.Button("Join the Discord", buttonSize))
+                Process.Start(new ProcessStartInfo { FileName = "https://discord.gg/KvGJCCnG8t", UseShellExecute = true });
+            ImGui.PopStyleColor();
+            ImGui.SameLine();
+
+            ImGui.PushStyleColor(ImGuiCol.Button, new Vector4(0, 100, 255, 255) / 255);
+            if (ImGui.Button("View my Twitter", buttonSize))
+                Process.Start(new ProcessStartInfo { FileName = "https://twitter.com/MiniatureMoosey", UseShellExecute = true });
+            ImGui.PopStyleColor();
+            ImGui.SameLine();
+        }
+    }
+}
---
+++ Dalamud/XAT.Plugin/XATPlugin.cs
@@ -0,0 +1,115 @@
+﻿using Dalamud.Game;
+using Dalamud.Game.ClientState.Objects;
+using Dalamud.Game.Command;
+using Dalamud.Interface.Windowing;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+using XAT.Plugin.UI;
+
+namespace XAT.Plugin;
+
+public class XATPlugin : IDalamudPlugin
+{
+    public const string PluginName = "XAT";
+    public string Name => PluginName;
+
+    private const string CommandName = "/xat";
+
+    [PluginService]
+    public ICommandManager CommandManager { get; init; } = null!;
+
+    [PluginService]
+    public IFramework Framework { get; init; } = null!;
+
+    [PluginService]
+    public ISigScanner SigScanner { get; init; } = null!;
+
+    [PluginService]
+    public IObjectTable ObjectTable { get; init; } = null!;
+
+    [PluginService]
+    public ITargetManager TargetManager { get; init; } = null!;
+
+    [PluginService]
+    public IGameInteropProvider GameInteropProvider { get; init; } = null!;
+
+    [PluginService]
+    public IClientState ClientState { get; init; } = null!;
+
+    public IDalamudPluginInterface PluginInterface { get; }
+
+    public XATWindow Window { get; }
+    public WindowSystem WindowSystem { get; }
+
+    public bool IsInGPose { get; private set; }
+
+    public XATPlugin(IDalamudPluginInterface pluginInterface)
+    {
+        this.PluginInterface = pluginInterface;
+        this.PluginInterface.Inject(this);
+
+        this.PluginInterface.UiBuilder.DisableGposeUiHide = true;
+        this.PluginInterface.UiBuilder.DisableCutsceneUiHide = true;
+        this.PluginInterface.UiBuilder.Draw += DrawUI;
+        this.Framework.Update += Update;
+
+        this.CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
+        {
+            HelpMessage = "A useful message to display in /xlhelp"
+        });
+
+        this.Window = new(this);
+        this.WindowSystem = new(PluginName);
+        this.WindowSystem.AddWindow(Window);
+
+        Window.IsOpen = true;
+    }
+
+    private void OnCommand(string command, string args)
+    {
+        Window? window = this.Window;
+
+        if (window != null)
+        {
+            if (window.IsOpen)
+            {
+                window.IsOpen = false;
+            }
+            else
+            {
+                window.IsOpen = true;
+            }
+        }
+    }
+
+    private void DrawUI()
+    {
+        this.WindowSystem.Draw();
+    }
+
+    private void Update(IFramework framework)
+    {
+        if (ClientState.IsGPosing && IsInGPose == false)
+        {
+            IsInGPose = true;
+
+            Window.IsOpen = true;
+        }
+        else if (ClientState.IsGPosing == false && IsInGPose)
+        {
+            IsInGPose = false;
+        }
+    }
+
+    public void Dispose()
+    {
+        Framework.Update -= Update;
+
+        this.CommandManager.RemoveHandler(CommandName);
+
+        this.PluginInterface.UiBuilder.Draw -= DrawUI;
+
+        this.WindowSystem.RemoveAllWindows();
+    }
+}
