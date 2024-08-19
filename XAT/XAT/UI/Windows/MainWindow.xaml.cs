9fac12f759bcf9d68b0d6d6ff2337a6922d80bdf
Kenneth M <KennethMizell@outlook.com>
2024-08-19 04:01:47 -0500

    Make Snackbar popup take 10 seconds to dismiss

--- XAT/XAT/UI/Windows/MainWindow.xaml.cs
+++ XAT/XAT/UI/Windows/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using XAT.UI.Utils;
 
 namespace XAT.UI.Windows;
@@ -8,6 +9,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        MainSnackbar.MessageQueue = new MaterialDesignThemes.Wpf.SnackbarMessageQueue(TimeSpan.FromMilliseconds(10000));
         DialogUtils.MainSnackbar = this.MainSnackbar;
     }
 
